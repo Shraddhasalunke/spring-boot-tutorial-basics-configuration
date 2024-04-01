f0e952f584ebf3d66f13d5a6c00c19256ff2d2f5
Shraddha Salunke <91833741+Shraddhasalunke@users.noreply.github.com>
2024-04-01 17:16:29 +0530

    Add files via upload

---
+++ src/main/java/com/in28minutes/springboot/tutorial/basics/application/configuration/WelcomeResource.java
@@ -0,0 +1,37 @@
+package com.in28minutes.springboot.tutorial.basics.application.configuration;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeResource {
+
+    @Value("${welcome.message}")
+    private String welcomeMessage;
+
+    @Autowired
+    private BasicConfiguration configuration;
+
+    @GetMapping("/welcome")
+    public String retrieveWelcomeMessage() {
+        // Complex Method
+        return welcomeMessage;
+    }
+
+    @RequestMapping("/dynamic-configuration")
+    public Map<String, Object> dynamicConfiguration() {
+        // Not the best practice to use a map to store different types!
+        Map<String, Object> configMap = new HashMap<>();
+        configMap.put("message", configuration.getMessage());
+        configMap.put("number", configuration.getNumber());
+        configMap.put("key", configuration.isValue());
+
+        return configMap;
+    }
+}
